ce90a1e18b5f5caff7258ed3e06305babc659526
celFree <cl2512486@hotmail.com>
2025-10-29 10:58:18 +1100

    idk
    idk

--- action.js
+++ action.js
@@ -5,7 +5,7 @@ function refreshSplash() {
 
     if(index == texts.length) {
         main_splash.textContent = "You didn't like the splash message I gave you?!";
-        second_splash.textContent = "ungrateful bitch."
+        second_splash.textContent = "ungrateful."
         current_splash.value = -1;
         return;
     }
@@ -14,6 +14,7 @@ function refreshSplash() {
     applyNewSplash();
 }
 
+// TODO: delete this useless trash
 function fakeReload() {
     alert("just use the re-load button at the top of browser!!!\nit comes FREE!! with your browser!!!!!!!11!!1!!");
 }
@@ -34,29 +35,16 @@ function requestSplash() {
 
 function incrementSplash(amount) {
     if(typeof amount != "number")
-        throw TypeError("why the fuck do you want to increment by a " + typeof amount);
+        throw TypeError("why the hell do you want to increment by a " + typeof amount);
 
     current_splash.value += amount;
     applyNewSplash();
 }
 
-function toggleSuggestive() {
-    if(localStorage.getItem("suggestive") == null) {
-        if(sexcheck.checked)
-            localStorage.setItem("suggestive", "no I am a virgin"); // unnecessarily long string value haha ik bad practise but fuck you
-        else
-            localStorage.setItem("suggestive", "yes I like r34");
-    } else {
-        
-    }
-}
-
-
-
 main_splash.addEventListener("click", refreshSplash);
 second_splash.addEventListener("click", refreshSplash);
 document.getElementById("regen_splash").addEventListener("click", requestSplash);
 document.getElementById("requested_splash").addEventListener("input", () => current_splash.value = parseInt(document.getElementById("requested_splash").value));
-document.getElementById("previous_splash").addEventListener("click", () => {if(texts[current_splash.value - 1][2] == true && localStorage.getItem("suggestive") == "no I am a virgin") incrementSplash(-2); else incrementSplash(-1)});
-document.getElementById("next_splash").addEventListener("click", () => {if(texts[current_splash.value + 1][2] == true && localStorage.getItem("suggestive") == "no I am a virgin") incrementSplash(2); else incrementSplash(1)});
+document.getElementById("previous_splash").addEventListener("click", () => incrementSplash(-1));
+document.getElementById("next_splash").addEventListener("click", () => incrementSplash(1));
 sexcheck.addEventListener("change", toggleSuggestive)
